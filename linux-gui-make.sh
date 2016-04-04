cacf2eb626137b8f1701951c7cd2e77314367503
clairewiwi <clairewiwi@gmail.com>
2016-04-04 17:27:25 +0200

    Added files via upload

---
+++ linux-gui-make.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+#echo [bin-linux/gui] building SDL1.2 gui binary..
+#gcc gui/*.c lib/src/*.c -I lib/include/ -lfreeimage -lstdc++ -lSDLmain -lSDL -o bin-linux/gui_sdl12
+
+echo [bin-linux/gui] building SDL2 gui binary...
+#objcopy -B i386 -I binary -O elf32-i386 res/ui.bmp ui_bmp.o
+objcopy -B i386 -I binary -O elf64-x86-64 res/ui.bmp ui_bmp.o
+gcc ui_bmp.o gui/*.c lib/src/*.c -I lib/include/ -lfreeimage -lstdc++ -lSDL2main -lSDL2 -DSDL2 -o bin-linux/gui
+rm ui_bmp.o
